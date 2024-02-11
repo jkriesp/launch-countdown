f4f9261e551e32d908a4900523005bac0ac2b8cc
Jens Kristian Espevik <jenskristian@jenske.net>
2024-02-11 12:44:08 +0100

    add number formatting for numbers less than 10

--- src/components/CountdownDisplay.tsx
+++ src/components/CountdownDisplay.tsx
@@ -5,12 +5,18 @@ interface CounterDisplayProps {
 
 const CountdownDisplay = ({ value, type }: CounterDisplayProps) => {
   const topValue = value; // This could be adjusted to show the next value for animation
-  const bottomValue = value;
+  const bottomValue = value; // todo: top and bottom values for animation
+  const formattedValue = formatNumber(value); // formatt values so that they are always 2 digits long
+
+  function formatNumber(number: number) {
+    // Check if the number is less than 10 and prepend a '0' if it is
+    return number < 10 ? `0${number}` : number.toString();
+  }
 
   return (
     <div className="countdown">
       <div className="numberCard">
-        <div className="number">{topValue}</div>
+        <div className="number">{formattedValue}</div>
         <div className="half top"></div>
         <div className="half bottom"></div>
       </div>
